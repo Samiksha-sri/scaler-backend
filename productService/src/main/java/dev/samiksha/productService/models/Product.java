542df81b91bf8a09f42a2a639f67f3700690dfde
Samiksha <srivastavasamiksha97@gmail.com>
2023-09-15 22:14:12 +0530

    DB operations

--- productService/src/main/java/dev/samiksha/productService/models/Product.java
+++ productService/src/main/java/dev/samiksha/productService/models/Product.java
@@ -1,9 +1,14 @@
 package dev.samiksha.productService.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
     private String image;
+    @ManyToOne
     private Category category;
     private double price;
 }
